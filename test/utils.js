18b85feae26cc6cdee2139cb0c6f14a76a82cb41
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2014-01-15 07:44:59 +0100

    fixed rangecheck

--- test/utils.js
+++ test/utils.js
@@ -2,31 +2,47 @@ var utils = require('../lib/utils/utils.js');
 
 exports.cell = {
 	isCellNotation: function(test){
-		var v = utils.isCellNotation('BY23');
+		var v;
+		test.expect(2);
+		v = utils.isCellNotation('BY23');
 		test.ok(v);
-		test.done();
-	},
-	isNotCellNotation: function(test){
-		var v = utils.isCellNotation('BY012');
+		v = utils.isCellNotation('BY012');
 		test.ok(!v);
 		test.done();
 	},
 	isRangeNotaion: function(test){
-		var v = utils.isRangeNotaion('BY12:BY1');
-		test.ok(v);
-		test.done();
-	},
-	isNotRangeNotaion: function(test){
-		var v = utils.isRangeNotaion('BY12:BZ1');
-		test.ok(!v);
+		var v;
+		test.expect(4);
+		v = utils.isRangeNotaion('BA12:BA1');
+		test.ok(v,"same column, differnt rows");
+		v = utils.isRangeNotaion('A1:Z1');
+		test.ok(v,"same row, different column");
+		v = utils.isRangeNotaion('A1:A1');
+		test.ok(v,"same row, same column");
+		v = utils.isRangeNotaion('BY12:BZ1');
+		test.ok(!v,"different columns and rows (matrix) should fail");
 		test.done();
 	},
 	getColumnAndRow: function(test){
-		var v = utils.getColumnAndRow('BA3');
+		var v;
+		test.expect(3);
+		v = utils.getColumnAndRow('B3');
 		test.deepEqual(v,{
 			column: 'B',
 			columnIndex: 2,
 			row: 3
+		});
+		v = utils.getColumnAndRow('AA999');
+		test.deepEqual(v,{
+			column: 'AA',
+			columnIndex: 27,
+			row: 999
+		});
+		v = utils.getColumnAndRow('BA1');
+		test.deepEqual(v,{
+			column: 'BA',
+			columnIndex: 53,
+			row: 1
 		});
 		test.done();
 	}
