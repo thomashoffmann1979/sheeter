077c1d777ec0cb8148a4f651c4ad717256d3324e
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2014-01-17 21:32:44 +0100

    added samples for min,max,avg and sum

--- sample/workbook_02.js
+++ sample/workbook_02.js
@@ -5,7 +5,7 @@ var workbook = new Workbook();
 var sheet = workbook.createWorkSheet();
 
 sheet.getCell('A1',{value: 5});
-sheet.getCell('A1').value=5;
+sheet.getCell('A1').value=1;
 sheet.getCell('A2',{value: 5});
 sheet.getCell('A3',{value: 5});
 sheet.getCell('A4',{value: 5});
@@ -15,3 +15,13 @@ console.log(sheet.getCell('A6').value);
 
 sheet.getCell('A7',{formula:'=AVG(A1:A5)'});
 console.log(sheet.getCell('A7').value);
+
+sheet.getCell('A7').formula ='=AVG(A1;A4;A5)';
+console.log(sheet.getCell('A7').value);
+
+
+sheet.getCell('A8').formula ='=Max(A1;A2)';
+console.log(sheet.getCell('A8').value);
+
+sheet.getCell('A9').formula ='=Min(A1:A2)';
+console.log(sheet.getCell('A9').value);
