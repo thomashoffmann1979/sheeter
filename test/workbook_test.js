a2f63595f1268a71f8e40e2ac5c71a64ef5c5da0
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2014-01-17 01:04:51 +0100

    added sum-function

--- test/workbook_test.js
+++ test/workbook_test.js
@@ -23,7 +23,7 @@ exports.workbook = {
 	},
 	if_function: function(test){
 		var v;
-		test.expect(3);
+		test.expect(4);
 		var workbook = new Workbook();
 		var sheet = workbook.createWorkSheet();
 		sheet.getCell('A1').value=1;
@@ -36,9 +36,11 @@ exports.workbook = {
 		sheet.getCell('B1').formula = '=IF(1=2;3;4)';
 		sheet.getCell('B2').formula = '=IF(1=2;3)';
 		sheet.getCell('B3').formula = '=IF(1=A1;A7;-1)';
+		sheet.getCell('B4').formula = '=SUM(A1:A3)';
 		test.ok(sheet.getCell('B1').value == 4);
 		test.ok(sheet.getCell('B2').value === '');
 		test.ok(sheet.getCell('B3').value == 0);
+		test.ok(sheet.getCell('B4').value == 2);
 		test.done();
 	},
 }
