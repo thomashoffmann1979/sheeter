80b539a03f6ae61995c5219a4b3e4d9691018114
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2014-01-20 19:07:04 +0100

    started xlsx reader

---
+++ sample/xlsx_reader_01.js
@@ -0,0 +1,14 @@
+var XLSXReader = require('../lib/reader/xlsxreader').XLSXReader,
+    path =  require('path'),
+    reader;
+
+
+reader = new XLSXReader({
+    filename: path.join(__dirname,'sample_01.xlsx')
+});
+
+reader.open({},function(err){
+    if (err) throw err;
+    console.log('done');
+});
+
--- test/cell_test.js
+++ test/cell_test.js
@@ -1,22 +1,21 @@
+"use strict";
 var Cell = require('../lib/cell/cell.js').Cell;
 
 exports.cell = {
-	setID: function(test){
-		var v;
-		test.expect(1);
-		var test_cell = new Cell();
-		test_cell.id = 'B3';
-		test.ok(test_cell.id==='B3');
-		test.done();
-	},
-	setFormula: function(test){
-		var v;
-		test.expect(1);
-		var test_cell = new Cell({
-			id: 'A1',
-			formula: '=2+1'
-		});
-		test.ok(test_cell.formula==='=2+1');
-		test.done();
-	}
-}
+    setID: function (test) {
+        var test_cell = new Cell();
+        test.expect(1);
+        test_cell.id = 'B3';
+        test.ok(test_cell.id === 'B3');
+        test.done();
+    },
+    setFormula: function (test) {
+        var test_cell = new Cell({
+            id: 'A1',
+            formula: '=2+1'
+        });
+        test.expect(1);
+        test.ok(test_cell.formula === '=2+1');
+        test.done();
+    }
+};
