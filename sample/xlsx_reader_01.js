b9fcf063c67dbc0a55be43b9fdfa4b3de0a6195b
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2014-01-22 06:59:33 +0100

    added nicer output

--- sample/xlsx_reader_01.js
+++ sample/xlsx_reader_01.js
@@ -1,14 +1,34 @@
 var XLSXReader = require('../lib/reader/xlsxreader').XLSXReader,
     path =  require('path'),
-    reader;
+    reader,
+    Workbook = require('../lib/workbook/workbook').Workbook;
 
 
 reader = new XLSXReader({
-    filename: path.join(__dirname,'sample_01.xlsx')
+    filename: path.join(__dirname,'sample_01.xlsx'),
+    workbook: new Workbook()
+});
+
+reader.on('sheetsReady', function(){
+    var list = reader.workbook.getWorkSheetList(),
+        i,
+        m,
+        r;
+    
+    for(i in list){
+        m = list[i].getMatrixByRow({nice:true,header:true,rows:true});
+        console.log(list[i].title+':');
+        for( r in m ){
+            console.log('|'+m[r].join('|')+'|');
+            if ( i == 0){
+                
+            }
+        }
+    }
+
 });
 
 reader.open({},function(err){
     if (err) throw err;
     //console.log('done');
 });
-
